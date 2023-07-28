a10b40a70673bb7d9efc04960fe594913334272b
Hope <suhova1313@gmail.com>
2023-07-28 17:37:21 +0400

    missed letter in h2

--- Functions.js
+++ Functions.js
@@ -30,7 +30,7 @@ function NewLine(text) {
     word = 1;
     for (let i = 5; i < text.length - 1; i++) {
         if (text[i] == '\n') {
-            list += '<a onclick=\" LoadRecipe(\'' + text.substring(word+1, i - 1) + '\')\" href=\"#' + text.substring(word, i - 1) + '\">' + text.substring(word, i) + '</a>'
+            list += '<a onclick=\" LoadRecipe(\'' + text.substring(word, i) + '\')\" href=\"#' + text.substring(word, i - 1) + '\">' + text.substring(word, i) + '</a>'
             word = i;
 
         }
